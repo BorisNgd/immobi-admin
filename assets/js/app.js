334782f62d1b1d054fe40cf38c2c152d88deefce
BorisNgd <bngd228@gmail.com>
2020-06-30 02:16:46 +0100

    Remove fortawesome package

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,6 @@ require('jquery-ui-bundle');
 import  'bootstrap';
 require('datatables.net-bs4');
 import 'bootstrap4-toggle/css/bootstrap4-toggle.min.css'
-import '@fortawesome/fontawesome-free/js/all';
 import '../css/custom.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
